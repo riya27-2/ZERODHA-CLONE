3ef8a8e838d6a16abab0e6416f6a07db15edd6cf
unknown <riyapatel7228@gmail.com>
2026-04-14 12:44:46 +0530

    added order feature

--- dashboard/src/components/Orders.js
+++ dashboard/src/components/Orders.js
@@ -7,7 +7,7 @@ const Orders = () => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    axios.get("http://localhost:8080/allOrders")
+    axios.get("http://localhost:3002/allOrder")
       .then(res => {
         setOrders(res.data);
         setLoading(false);
